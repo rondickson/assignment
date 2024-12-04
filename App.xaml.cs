22ac5b30b512f0ffab7944141ddf58989d10f883
rondickson <146130094+rondickson@users.noreply.github.com>
2024-12-03 23:14:10 -0600

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,16 @@
-﻿using CSC317PassManagerP2Starter.Modules.Controllers;
+using CSC317PassManagerP2Starter.Modules.Controllers;
 using CSC317PassManagerP2Starter.Modules.Views;
 
 namespace CSC317PassManagerP2Starter
 {
     public partial class App : Application
     {
-        public static LoginController LoginController = new LoginController();
-        public static PasswordController PasswordController = new PasswordController();
+        public static LoginController login_controller = new();
+        public static PasswordController password_controller = new();
 
         public App()
         {
             InitializeComponent();
-
             MainPage = new LoginView();
         }
     }
